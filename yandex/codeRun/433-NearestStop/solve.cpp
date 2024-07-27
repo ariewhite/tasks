a91661b6a6ceddf6a209d80204984924839078a7
ariewhite <arie.white@bk.ru>
2024-07-27 22:20:35 +0500

    433

--- yandex/codeRun/433-NearestStop/solve.cpp
+++ yandex/codeRun/433-NearestStop/solve.cpp
@@ -1,48 +1,81 @@
 #include <iostream>
-#include <chrono>
 #include <vector>
-#include <set>
 
 /*
     input:
-    3 2 // n & k 
+    3 2 // n & k
     1 3 5 // координаты остановок
     4 1 // запросы
 
-    a:
-    num - координата автобуса
-    ближайшая остановка находится по формуле:
-        min((right - num), (num - left))
-    если расстояния до правой и левой остановки равны
+    решение:
+    запишем все данные с координатами остановок в вектор, т.к данные отсортированы
+    бинарный поиск для поиска ближайших остановок так и просится
+    сложность O(log(N))
+    
+    алгортим обработки нового добавленно запроса:
+    1. бинарный поиск исчет ближайший элемент к num
+    2. если элементов слева нет, то выводится ближайший правый
+       если элементы слева есть, выводится ближайший левый
+       
 
-    
-    
+
 */
 int main()
 {
     unsigned int n, k;
     std::cin >> n >> k;
 
-    std::set<int> * set = new std::set<int>;
-    std::vector<int>    * ai  = new std::vector<int>;
-    //std::vector<int> * coords = new std::vector<int>;
+    std::vector<int> *ai = new std::vector<int>;
+    // std::vector<int> * coords = new std::vector<int>;
+    // std::set<int> * set = new std::set<int>;
 
     for (size_t i = 0; i < n; ++i) // fell a(i)
     {
-        int num; 
+        int num;
         std::cin >> num;
-        //ai->push_back(num);
-        set->insert(num);
+        ai->push_back(num);
     }
-    for (size_t i = 0; i < k; ++i) // fell bus coordina 
+    for (size_t i = 0; i < k; ++i) // fell bus coordinates
     {
         int num;
         std::cin >> num;
-        if (set->contains(num))
+
+        int low = 0; 
+        int high = ai->size() - 1;
+        int close = -1;
+        int closeRight = -1;
+
+        while (low <= high)
         {
-            std::cout << num << " ";
-        }    
+            int middle = low + (high - low) / 2;
+            //std::cout << "m - " << middle << " ";
+
+            if (num == (*ai)[middle]){
+                close = middle;
+                break;
+            }
+            if (num > (*ai)[middle]){
+                close = middle;
+                low = middle + 1;
+            }
+            if (num < (*ai)[middle]){
+                high = middle - 1;
+            }
+        }
+        //std::cout << std::endl;
+
+        closeRight = close + 1;
+
+        if (close == -1){
+            //std::cout << "right" << std::endl;
+            std::cout << closeRight + 1<< std::endl;
+        } else {
+            //std::cout << "left" << std::endl;
+            std::cout << close + 1 << std::endl;
+        }
     }
 
-    std::cout << 0;ds
+    delete ai;
 }
+
+// 1 3 5 7 9 10 13 14 17 20 24 25 27 28
